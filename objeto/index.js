91010732c7aa89815ceeeb3263e088eccc1120f4
janiel <janielabreu309@gmail.com>
2025-04-01 11:24:04 -0400

    subi el ejercicio objeto

---
+++ objeto/index.js
@@ -0,0 +1,31 @@
+const user = {
+    name: 'janiel',
+    lastname: 'abreu',
+    ege: 21,
+    addres: {
+        country: "republica dominicana",
+        city: "santo domingo",
+        street: "la paz c.2c #22"
+    },
+    friends: ['calvin','yonael','andier'],
+    active: true,
+    sendmail(){
+        return 'sending email'
+    }
+}
+
+// console.log(user.addres.city);
+
+
+// propieda
+
+const name = "lapto"
+const price = 3000
+
+const newproduct ={
+    name,
+    price
+}
+
+console.log(newproduct);
+
